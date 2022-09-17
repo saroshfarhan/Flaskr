738e9074a4baab90f5ee81f686c82f255289c897
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-09-17 20:34:58 +0530

    added pytests

---
+++ tests/test_db.py
@@ -0,0 +1,30 @@
+import sqlite3
+from unittest import result
+
+import pytest
+from flaskr.database import get_db
+
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+    
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+    
+    assert 'closed' in str(e.value)
+
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+    
+    monkeypatch.setattr('flaskr.database.init.db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
+
+
